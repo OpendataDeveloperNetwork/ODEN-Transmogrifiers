ef759df4261c8bc9b31f727bba34d7a4a98924cd
Markus Sample <markussample@gmail.com>
2023-04-25 17:28:26 -0700

    add basic files

---
+++ filters/canada/british_columbia/vancouver/public_art/json_to_json.js
@@ -0,0 +1,17 @@
+const filter = function(data) {
+    let new_data = [];
+    data = JSON.parse(data);
+
+    // pull schema?
+    
+    data.map(d => {
+        let item = {};
+        item.name = d.title_of_work;
+        item.location = d.geo_point_2d;
+        new_data.push(item);
+    })
+
+    // validate with schema?
+
+    return JSON.stringify(new_data, null);
+}
