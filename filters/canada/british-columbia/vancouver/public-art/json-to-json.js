847f439c1308f00e98327ea707d875ffc6501e66
Markus Sample <markussample@gmail.com>
2023-04-25 19:08:00 -0700

    conform to schema

--- filters/canada/british-columbia/vancouver/public-art/json-to-json.js
+++ filters/canada/british-columbia/vancouver/public-art/json-to-json.js
@@ -1,23 +1,23 @@
-const filter = async function(axios, Validator, data) {
+const filter = async function (axios, Validator, data) {
     let new_data = [];
     data = JSON.parse(data);
 
-    let schema = await axios({
-        url: "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/prototype/schemas/public_art.schema.json", 
+    schema = await axios({
+        url: "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/prototype/schemas/public-art.schema.json",
         method: 'GET',
         responseType: 'blob',
     })
     schema = JSON.parse(schema.data);
-    
+
+    let v = new Validator();
     data.map(d => {
         let item = {};
         item.name = d.title_of_work;
-        item.location = d.geo_point_2d;
+        let coordinates = { longitude: d.geo_point_2d.lon, latitude: d.geo_point_2d.lat };
+        item.coordinates = coordinates;
+        v.validate(item, schema, { required: true, throwFirst: true });
         new_data.push(item);
     })
-
-    let v = new Validator();
-    v.validate(new_data, schema, {required: true, throwFirst: true});
 
     return JSON.stringify(new_data, null);
 }
