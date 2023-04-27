82090abcd1aeab17994ab5263267901e08f0a68f
Sarah Semkow <ssemkow@my.bcit.ca>
2023-04-27 15:40:20 -0700

    changed function name to null_check, changed to add data to errors array, changed to now treat data_field as the data that is null/not null/empty/undefined

--- libraries/standard.js
+++ libraries/standard.js
@@ -3,27 +3,27 @@ const lib = new Map();
 // check if read values are null, undefined or blank (empty string)
 // dst: destination object, data: data item being processed, 
 // data_field: field being processed, skip_errors: boolean, errors: array of bad data items
-const null_check_function = (dst, data, data_field, skip_errors, errors) => {
-  // data or data_field are null or undefined, or data is empty object
-  if (!data || Object.keys(data).length === 0 || !data_field) {
+const null_check = (dst, data, data_field, skip_errors, errors) => {
+  // data is null, undefined or an empty object
+  if (!data || Object.keys(data).length === 0) {
     console.log("No data or data field supplied")
     return false
   }
-  if (data[data_field]) {
-    dst = data[data_field]
-    console.log(`${data[data_field]} has been added to the destination object.`)
+  if (data_field) {
+    dst = data_field
+    console.log(`${data_field} has been added to the destination object.`)
     return true
   }
-  // null data[data_field] and skip errors
-  if (!data[data_field] && skip_errors) {
+  // null data_field and skip errors
+  if (!data_field && skip_errors) {
     console.log(`A required value was undefined in the data. The field ${data_field} was skipped.`)
   } else {
-    // null data[data_field] and no skip errors
-    errors.push(data_field)
+    // null data_field and no skip errors
+    errors.push(data)
     console.log(`A required value was undefined in the data. The field ${data_field} was added to the errors array.`)
   }  
 }
-lib.set("null_check_function", null_check_function);
+lib.set("null_check", null_check);
 
 // const example_function = function(example_parameter) {
 //     console.log("some example behaviour " + example_parameter);
