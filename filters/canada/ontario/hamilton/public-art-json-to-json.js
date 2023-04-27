1f40ddc5df312b3b93fb9d379c1a841398612c8f
Urjit Sharma <urjitsharma12@gmail.com>
2023-04-27 13:59:41 -0700

    Implemented filter for hamilton

---
+++ filters/canada/ontario/hamilton/public-art-json-to-json.js
@@ -0,0 +1,37 @@
+const filter = function (data, stringify) {
+    if (typeof data === 'string' || data instanceof String) {
+        data = JSON.parse(data);
+    }
+    
+    let new_data = [];
+
+    data["features"].map(d => {
+        let item = {};
+        item.name = d.attributes["ARTWORK_TITLE"];
+        if (item.name === undefined) {
+            console.log(`Data name not found for art with url ${d.url}`);
+        }
+        let coordinates = { longitude: d.attributes["LONGITUDE"], latitude: d.attributes["LATITUDE"]};
+        if (coordinates.longitude === undefined || coordinates.latitude === undefined) {
+            console.log(`Data coordinates not found for art with url ${d.url}`);
+        }
+        item.coordinates = coordinates;
+
+        let details = {};
+        if (d.attributes["DESCRIPTION"] != null || d.attributes["DESCRIPTION"] != undefined) {
+            details.description = d.attributes["DESCRIPTION"];
+        }
+        details.location = d.attributes["STREET"];
+
+        item.details = details;
+        new_data.push(item);
+    })
+
+    if (stringify) {
+        return JSON.stringify(new_data, null);
+    }
+
+    return new_data;
+}
+
+return filter;
