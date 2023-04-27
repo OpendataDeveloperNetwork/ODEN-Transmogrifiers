8fb12af55cfcd32929fc2f34c23193d1aa30035e
Brian Hsieh <bhsieh7@my.bcit.ca>
2023-04-27 13:56:46 -0700

    Updated filter parameters to include std_lib and skip_errors flag

--- filters/canada/ontario/mississauga/public-art-geojson-to-json.js
+++ filters/canada/ontario/mississauga/public-art-geojson-to-json.js
@@ -1,4 +1,4 @@
-const filter = function (data, stringify) {
+const filter = function (data, std_lib, stringify, skip_errors) {
     if (typeof data === 'string' || data instanceof String) {
         data = JSON.parse(data);
     }
