454067dc5440d21df27cb6db66169907a52d5ed4
Markus Sample <markussample@gmail.com>
2023-05-04 13:19:45 -0700

    update null filter

--- filters/null-json-to-json.js
+++ filters/null-json-to-json.js
@@ -1,10 +1,31 @@
-const filter = function (data, std_lib, stringify, skip_errors) {
+const filter = function (data, std_lib, schema, validator, stringify) {
     if (typeof data === 'string' || data instanceof String) {
         data = JSON.parse(data);
     }
-    if (stringify) {
-        return JSON.stringify(data, null);
+
+    if (!std_lib) {
+        throw "standard library not provided";
     }
-    return data;
+    let validate_params = std_lib.get("validate_params");
+
+    // validate parameters object
+    schema = validate_params(schema, validator);
+
+    let new_data = [];
+    let errors = [];
+
+    data.map(d => {
+        let result = validator.validate(item, schema, { required: true });
+        if (!result.valid) {
+            errors.push({ type: "validation", validation_result: result, data_entry: d })
+        } else {
+            new_data.push(item);
+        }
+    })
+
+    if (stringify) {
+        new_data = JSON.stringify(new_data, null);
+    }
+    return { data: new_data, errors: errors };
 }
 return filter;
