bfc20556c23068e7c91a2bc3ebaf4fb6239ca83a
Brian Hsieh <bhsieh7@my.bcit.ca>
2023-05-10 13:51:57 -0700

    Added comments to json collector

--- collectors/collector-json.js
+++ collectors/collector-json.js
@@ -1,11 +1,15 @@
 const collector_json = function (data, params) {
+    // convert JSON data string to object form
     if (typeof data === 'string' || data instanceof String) {
         data = JSON.parse(data);
     }
 
+    // create combined data and errors arrays
     let combined_data = [];
     let combined_errors = [];
 
+    // for each entry in the data go through valid entries and errors and add
+    // them to the combined arrays
     data.map(d => {
         d.data.map(valid_entry => {
             combined_data.push(valid_entry)
@@ -15,8 +19,10 @@ const collector_json = function (data, params) {
         })
     })
 
+    // add the combined data and errors into one object
     let data_and_errors = {data: combined_data, errors: combined_errors};
 
+    // stringify in different formats depending on params
     if (params["stringify"] === true && params["indent"] === true) {
         data_and_errors = JSON.stringify(data_and_errors, null, "    ");
     }else if (params["stringify"] === true && !params.indent) {
