7f32557747db89c72b732e6511521f1dabd4eb12
Markus Sample <markussample@gmail.com>
2023-05-14 16:16:58 -0700

    update ODEN filter

--- filters/oden-to-transmog.js
+++ filters/oden-to-transmog.js
@@ -1,9 +1,23 @@
+const library_url = "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/dev/libraries/standard.js";
+const collector_url = "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/dev/collectors/collector-json.js";
+const label_url = "https://github.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/blob/dev/filters/label.js";
+const validate_url = "https://github.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/blob/dev/filters/validate.js";
+const strigify_url = "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/dev/filters/stringify.js";
+
 const filter = function (data, params) {
     if (typeof data === 'string' || data instanceof String) {
         data = JSON.parse(data);
     }
 
     let output_obj = {schema: "", entries: []};
+    
+    collector_filter = {"func": collector_url};
+    validate_filter = {"func": validate_url, params: {"validator": "json"}};
+    strigify_filter = {"func": strigify_url, params: {indent: true}};
+    output_obj.filters = [collector_filter, validate_filter, strigify_filter];
+
+    output_obj.sinks = [{"func": "file_write", params: {"path": "public-art-data.json"}}];
+
     let category = params.category
     data.map((item) => {
         if (item.labels.category === category) {
@@ -14,7 +28,7 @@ const filter = function (data, params) {
                 entry_obj.source = {func: "url_read", params: {path: item.data.datasets.json.url}};
             }
             if (item.data.datasets.json.filters.json){
-                entry_obj.filters = {func: item.data.datasets.json.filters.json, params:{library: "https://raw.githubusercontent.com/OpendataDeveloperNetwork/ODEN-Transmogrifiers/dev/libraries/standard.js"}};
+                entry_obj.filters = {func: item.data.datasets.json.filters.json, params:{library: library_url}};
             }
             output_obj.entries.push(entry_obj);
         }
