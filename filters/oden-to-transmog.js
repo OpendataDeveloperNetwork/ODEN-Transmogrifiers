7bc82cb7b1d43419629a50919ed06dcd94304814
Markus Sample <markussample@gmail.com>
2023-05-16 13:31:59 -0700

    test1

--- filters/oden-to-transmog.js
+++ filters/oden-to-transmog.js
@@ -25,29 +25,36 @@ const filter = function (data, params) {
     let category = params.category
     data.map((item) => {
         if (item.labels.category === category) {
+            let skip = false;
             schema_entry_obj.schema = item.data.schema;
 
             let entry_obj = {};
             
             // source
-            if (item.data.datasets.json.url){
+            if (item.data.datasets.json?.url){
                 entry_obj.source = {func: "url_read", params: {path: item.data.datasets.json.url}};
+            } else {
+                skip = true;
             }
             
             // filters
             entry_obj.filters = [];
             
             // standardize filter
-            if (item.data.datasets.json.filters.json){
+            if (item.data.datasets.json?.filters?.json){
                 standardize_filter = {func: item.data.datasets.json.filters.json, params:{"library": library_url}};
                 entry_obj.filters.push(standardize_filter);
+            } else {
+                skip = true;
             }
 
             // label filter
             label_filter = {func: label_url, params: {labels: item.labels}};
             entry_obj.filters.push(label_filter);
             
-            schema_entry_obj.entries.push(entry_obj);
+            if (!skip) {
+                schema_entry_obj.entries.push(entry_obj);
+            }
         }
     })
     return schema_entry_obj;
