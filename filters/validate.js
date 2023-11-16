a9a0c579df17d326c121f181621f3a59d19fc764
FelixNg21 <31874436+FelixNg21@users.noreply.github.com>
2023-11-15 22:53:32 -0800

    Update validate.js

--- filters/validate.js
+++ filters/validate.js
@@ -1,21 +1,32 @@
 const filter = function (data, params) {
-    console.log("data:", data)
-    console.log("params:", params)
     // check for schema
     if (!params.schema) {
         throw "schema not found";
     }
 
-    // detect the schema type, and get the corresponding validator
+    // detect schema type and parse schema
     let schema;
     let validator;
-    let type;
+    let schemaType;
+    let validatorType;
     try {
         let json_schema = JSON.parse(params.schema);
         schema = json_schema;
-        type = "JSON";
-        validator = params["JSON-validator"];
+        schemaType = "json";
     } catch { };
+
+    // get validator for schema type
+    switch (schemaType) {
+        case "json": {
+            if (params["jsonschema"]) {
+                validator = params["jsonschema"];
+                validatorType = "jsonschema";
+            } else if (params["ajv"]) {
+                validator = params["ajv"].compile(schema);
+                validatorType = "ajv";
+            }
+        }
+    }
 
     // check for unsupported schema type (!schema) or missing validator for schema type (!validator)
     if (!schema) {
@@ -24,16 +35,33 @@ const filter = function (data, params) {
     if (!validator) {
         throw "validate: validator for schema type not found"
     }
-
+    console.log("passed check")
     let valid_data = [];
     data.data.map(d => {
-        switch (type) {
-            case "JSON": {
-                let result = validator.validate(d, schema, { required: true });
-                if (!result.valid) {
-                    data.errors.push({ type: "validate-json", validation_result: result, data_entry: d })
-                } else {
-                    valid_data.push(d);
+        switch (schemaType) {
+            case "json": {
+                switch (validatorType) {
+                    case "jsonschema": {
+                        let result = validator.validate(d, schema, { required: true });
+                        if (!result.valid) {
+                            data.errors.push({ type: "validate-json", validation_result: result, data_entry: d });
+                        } else {
+                            valid_data.push(d);
+                        }
+                        break;
+                    }
+                    case "ajv": {
+                        let valid = validator(d);
+                        if (!valid) {
+                            data.errors.push({ type: "validate-json", validation_result: validator.errors, data_entry: d });
+                        } else {
+                            valid_data.push(d);
+                        }
+                        break;
+                    }
+                    default: {
+                        throw "validate: validator not supported"
+                    }
                 }
                 break;
             }
