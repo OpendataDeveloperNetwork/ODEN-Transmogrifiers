feb1b76ce16bff1bde6bb4d3bf0c26ba00226013
FelixNg21 <31874436+FelixNg21@users.noreply.github.com>
2023-11-15 21:14:33 -0800

    Update collector-json.js

--- collectors/collector-json.js
+++ collectors/collector-json.js
@@ -7,6 +7,7 @@ const collector_json = async function (datas, params) {
 
     // convert JSON data string to object form
     for (let [i, data] of datas.entries()) {
+        console.log(type of data)
         if (typeof data === 'string' || data instanceof String) {
             datas[i] = JSON.parse(data);
         }
